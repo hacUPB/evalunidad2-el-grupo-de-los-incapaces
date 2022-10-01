56e01bbac5c9ec51cbed727cc8c2d2ec77d7e1f5
SistComp <sistcomp@pop-os.localdomain>
2022-10-01 00:20:41 -0500

    Funciona hasta el 4

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -40,17 +40,48 @@ void AddEvent(EventList *this, Event *event)
 
 void RemoveEvent(EventList *this, char *name)
 {
+    Event *contador =this->head;
 
+    if(strcmp(this->head->eventName, name) == 0)//compara estructuras
+    {
+         
+        this->head = this->head->next;  // le asigna al 2do al HEAD
+        DestroyEvent(contador);   // destruye el primer evento
+    }
+    else if(strcmp(this->last->eventName, name) == 0)
+    {
+        while(contador->next != NULL)
+        {
+            if(contador->next->next == NULL)
+            {
+                this->last = contador->next;
+                contador->next = NULL; 
+                
+
+                break;
+            }
+            contador = contador->next;
+            
+        }
+        DestroyEvent(this->last);
+    }
+    
 }
 
 void ListEvents(EventList *this)
 {
-    Event *contador = this->head;
-    while(contador != NULL);
+    if(this->isEmpty == 1)
     {
-        printf("%s\n", contador->eventName);
-        contador = contador->next;
-    }   
-    //AYuADA diosA
-    //pedro perdon
+        printf("empty\n");
+    }
+    else
+    {
+        Event *contador = this->head;
+        while(contador != NULL)
+        { 
+            printf("%s\n", contador->eventName);
+            contador = contador->next;
+        }   
+    }
+    
 }
