5656e8f9c813a6f139aeb00692a4fcbfe412d2e9
SistComp <sistcomp@pop-os.localdomain>
2022-10-01 01:41:46 -0500

    mira ma sin manos, funciona hasta el 10

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -20,7 +20,7 @@ void DestroyEventList(EventList *this)
 
 Event *SearchEvent(EventList *this, char *name)
 {
-
+    
 }
 
 void AddEvent(EventList *this, Event *event)
@@ -41,31 +41,59 @@ void AddEvent(EventList *this, Event *event)
 void RemoveEvent(EventList *this, char *name)
 {
     Event *contador =this->head;
-
-    if(strcmp(this->head->eventName, name) == 0)//compara estructuras
+    if(this->isEmpty == 0)
     {
-         
-        this->head = this->head->next;  // le asigna al 2do al HEAD
-        DestroyEvent(contador);   // destruye el primer evento
-    }
-    else if(strcmp(this->last->eventName, name) == 0)
-    {
-        while(contador->next != NULL)
+        if(strcmp(this->head->eventName, name) == 0)//compara estructuras
         {
-            if(contador->next->next == NULL)
-            {
-                this->last = contador->next;
-                contador->next = NULL; 
-                
+            if(this->head->next == NULL )     
+            {   
+                this->isEmpty = 1;
+                this->head = NULL;
+                this->last = NULL;
 
-                break;
+                DestroyEvent(this->head);
             }
-            contador = contador->next;
+            else
+            {
+
+                this->head = this->head->next;  // le asigna al 2do al HEAD
+                DestroyEvent(contador);   // destruye el primer evento
+            }
+        }
+
+        else if(strcmp(this->last->eventName, name) == 0)
+        {
+            while(contador->next != NULL)
+            {
+                if(contador->next->next == NULL)
+                {
+                    this->last = contador->next;
+                    contador->next = NULL; 
+                    
+
+                    break;
+                }
+                contador = contador->next;
+                
+            }
+            DestroyEvent(this->last);
+        }
+        else
+        {
+            while (contador->next != NULL)
+            {
+                if(strcmp(name, contador->next->eventName) == 0){
+
+                    DestroyEvent(contador->next);
+                    contador->next = contador->next->next;
+                    
+                    break;
+                }
+                contador = contador->next;
+            }
             
         }
-        DestroyEvent(this->last);
     }
-    
 }
 
 void ListEvents(EventList *this)
