b88de19fa8ba8b91b49e8201a0239766fac2342f
SistComp <sistcomp@pop-os.localdomain>
2022-09-30 22:00:01 -0500

    PerdonDios

--- problem1/event.c
+++ problem1/event.c
@@ -4,10 +4,15 @@
 
 Event *CreateEvent(char *name)
 {
+    Event *event = malloc(sizeof(Event));
+    name[15]=0; //no puede tener un evento con mas de 15 caracteres
+    strncpy(event->eventName,name,strlen(name)+1); //nombre 
+    event->next = NULL; //next
 
+    return event;
 }
 
 void DestroyEvent(Event *this)
 {
-
+    free(this);
 }
--- problem1/eventList.c
+++ problem1/eventList.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+ 
 EventList *CreateEventList(void)
 {
+    EventList *eventList = malloc(sizeof(EventList));
+    eventList->isEmpty = 1;
+    eventList->head = NULL;
+    eventList->last = NULL;
 
-}
+    return eventList;   
+}   
 
 void DestroyEventList(EventList *this)
 {
-
+    free(this);
 }
 
 Event *SearchEvent(EventList *this, char *name)
@@ -20,14 +25,32 @@ Event *SearchEvent(EventList *this, char *name)
 
 void AddEvent(EventList *this, Event *event)
 {
+    if(this->isEmpty == 1){    
+        this->isEmpty = 0;
+        this->head = event;
+        this->last = event;
+    }
+    else{ 
 
+        this->last->next = event;
+        this->last = event;        
+    }
+    
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
+
 }
 
 void ListEvents(EventList *this)
 {
-
+    Event *contador = this->head;
+    while(contador != NULL);
+    {
+        printf("%s\n", contador->eventName);
+        contador = contador->next;
+    }   
+    //AYuADA diosA
+    //pedro perdon
 }
